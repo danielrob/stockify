8acd9fb4a2960e69b3e584e7d5b97a4a129670c1
Daniel Robinson <djr.self@gmail.com>
2016-04-11 09:52:01 -0400

    Improve image loading performance via preload, pre-decode

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
+  mainWindow.maximize();
+
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 
