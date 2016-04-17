0436616e15adda0ed9864fe900ecffa9cb2d12dc
Daniel Robinson <djr.self@gmail.com>
2016-04-17 14:27:43 -0400

    Refactor: Extract Services & better handle globals

--- app/directives.js
+++ app/directives.js
@@ -1,8 +1,4 @@
 "use strict";
-
-const
-   _ = require('underscore');
-
 angular.module('directives', [])
 
   .directive('fastImg', [function() {
---
+++ app/globals.js
@@ -0,0 +1,7 @@
+"use strict";
+const
+    _ = require('underscore'),
+    async = require('async'),
+    path = require('path'),
+    photoList = require('../lib/photoList'),
+    thumbnail = require('../lib/thumbnailer');
