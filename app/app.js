97112943e6b00e4ab0c22185625cde3cb1fe0b42
Daniel Robinson <djr.self@gmail.com>
2016-04-17 14:27:43 -0400

    Refactor: Extract Services & better handle globals

--- app/app.js
+++ app/app.js
@@ -1,92 +1,30 @@
 "use strict";
-const
-    path = require('path'),
-    lib = require('../lib/lib'),
-    thumbnailer = require('../lib/thumbnailer');
-
 angular.module('stockify-develop', ['services', 'directives'])
 
-  .controller('HomeCtrl', ['$scope', 'orientation',
-    function($scope, orientation) {
+  .controller('HomeCtrl', ['$scope', 'importService', 'scrollService',
+    function($scope, importService, scrollService) {
 
-      $scope.import = function(files){
-        lib.import(files, function(err, photos){
-          if (err) throw err;
-          if (photos.length === 0) return;
-          $scope.$broadcast('new-import');
-          postImport(photos);
-        });
-      }
-
-      function postImport(photos) {
-        // Update without a digest.
-        document.getElementById('import').textContent = "Importing";
+      function updateViewCallback(photos, callback) {
+        $scope.$broadcast('new-import');
         $scope.photoImport = photos;
         $scope.setSelectedRow(0);
+        $scope.initialized = true; // next $digest
+        document.getElementById('import').innerText = "Importing"; // immediately.
+      }
 
-        const startDate = new Date();
-
-        var count = 0;
-        async.each(photos, function(photo, next){
-          orientation(photo, function(o){
-            photo.orientation = o;
-            photo.orientClass = 'orient-' + o;
-            thumbnailer(photo, function(thumbpath){
-              photo.thumbnail = thumbpath;
-                if (count++ % 100 === 0) { // 100 seems a good performance heuristic
-                  $scope.initialized = true;
-                  $scope.$digest();
-                }
-                next();
-              });
-          })
-
-        }, function finallyDigest(){
-          console.log('\n Thumbnailing ' + photos.length + ' images took ' +
-                      (new Date().getTime() - startDate.getTime()) + ' milliseconds\n\n');
-          $scope.initialized = true;
+      $scope.import = function(files) {
+        importService.import(files, updateViewCallback, function() {
           $scope.$digest();
-        })
+        });
       }
 
       $scope.setSelectedRow = function(index) {
         $scope.selectedRow = index;
         $scope.$broadcast('new-photo-selected', index);
-
-        // Scrolling. Fine inline till it's not.
-        (function(document, index) {
-          var height = document.body.clientHeight;
-          var thumb = document.getElementById('anchor' + index);
-          var trail = document.getElementById('phototrail');
-          var filler = document.getElementById('scrollbar-filler');
-
-          if (!thumb) return;
-
-          // Bugfix. See below.
-          filler.style.display = "initial";
-          trail.style.overflowY = "hidden";
-
-          if (thumb.offsetTop - height + thumb.clientHeight > trail.scrollTop) {
-            trail.scrollTop = (thumb.offsetTop - height + thumb.clientHeight);
-          } else
-          if (thumb.offsetTop < trail.scrollTop) {
-            trail.scrollTop = thumb.offsetTop - 20;
-          }
-
-          /*
-            It's a bugfix. Without it, if there's been a click on the list,
-            chrome makes a small scrolling adjustment after the above,
-            giving it a horrible double-scroll-jerk.
-            This was the only way I could find to fix it.
-            The filler means we can still have a scrollbar, it hides the
-            fact the scrollbar temporarily dissapears during this process.
-          */
-          setTimeout(function() {
-            trail.style.overflowY = "scroll";
-            filler.style.display = "none";
-          }, 1);
-
-        })(document, index)
+        scrollService.scrollToIndex(index);
       }
+
+
+
     }
   ]);
--- app/services.js
+++ app/services.js
@@ -1,38 +1,137 @@
-"use strict";
-const
-   exif = require('exif').ExifImage,
-   async = require('async'),
-    q = async.queue(function (task, queueCallback) {
-    task.argz.push(queueCallback);
-    fetchOrientation.apply(null, task.argz);
-  }, 100);
+// required globals: photoList, thumbnail
 
 angular.module('services', [])
 
+  /*
+    Accepts an array of File Objects, gets the associated list of photos, and if it's not empty,
+    notifies the caller, and delegates the photoList to the photoProcessingService for processing.
+  */
+  .service('importService', ['photoProcessingService', function(photoProcessingService) {
 
-// https://gist.github.com/fisch0920/37bac5e741eaec60e983
-.service('orientation', function () {
-  return addToQueue;
-});
+    this.import = function(files, updateViewCallback, digestCallback) {
 
-function addToQueue(photo, haveOrientationCallback){
-  q.push({argz: [photo, haveOrientationCallback]});
-}
+      photoList(files, photoListHandler);
 
-function fetchOrientation(photo, haveOrientationCallback, queueCallback){
-    try {
-      new exif({ image: photo.path }, function(error, exifData) {
-        if (error) {
-          console.log('Error: ' + error.message);
-          haveOrientationCallback(1);
-          }
-        else {
-          queueCallback();
-          haveOrientationCallback(exifData.image.Orientation);
-        }
-      });
-    } catch (error) {
-      console.log('Error: ' + error.message);
-      haveOrientationCallback(1);
+      function photoListHandler(err, photos) {
+        if (err) throw err;
+        if (photos.length === 0) return;
+        updateViewCallback(photos);
+        photoProcessingService.process(photos, digestCallback);
+      }
     }
-  }
+
+  }])
+
+  /*
+    Accepts an array of photo objects (minimally {path: 'path/to/photo'}), and applies tasks which
+    add keys to the photo object. E.g.
+    {path: 'path/to/photo'} -> 
+    {path: 'path/to/photo', thumbnailPath: 'path/to/photoThumbnail', orientaion: 6}
+  */
+  .service('photoProcessingService', ['orientation', function(orientation) {
+
+    this.process =
+
+    function (photos, digestCallback) {
+      var count = 0;
+      const timerStart = new Date(),
+        q = async.queue(processPhoto, 100);
+
+      // Queue actions
+      function processPhoto(photo, callback) {
+        async.applyEachSeries(
+          [
+            orientation,
+            thumbnail
+          ],
+          photo,
+          function done() {
+            if (count++ % 100 === 0 || count === 20) digestCallback(); // 100 seems a good performance heuristic
+            callback();
+          }
+        );
+      }
+
+      // When empty
+      q.drain = function() {
+        console.log('\n Thumbnailing ' + photos.length + ' images took ' +
+                     (new Date().getTime() - timerStart.getTime()) + ' milliseconds\n\n');
+        digestCallback();
+      }
+
+      // Add all photos
+      q.push(photos)
+    }
+  }])
+
+  /*
+    Adds the 'orientation' and 'orientClass' keys to photo objects.
+  */
+  .factory('orientation', function() {
+
+    const
+      exif = require('exif').ExifImage;
+
+    // Source: https://gist.github.com/fisch0920/37bac5e741eaec60e983
+    return function fetchOrientation(photo, callback) {
+      if (photo.orientation !== void 0) return;
+
+      try {
+        new exif({ image: photo.path }, function(error, exifData) {
+          if (error) {
+            console.log('Error: ' + error.message);
+            photo.orientation = 1;
+            photo.orientClass = 'orient-1'
+            callback(null);
+          }
+          else {
+            photo.orientation = exifData.image.Orientation;
+            photo.orientClass = 'orient-' + exifData.image.Orientation;
+            callback(null);
+          }
+        });
+      } catch (error) {
+        console.log('Error: ' + error.message);
+        callback(null);
+      }
+    }
+  })
+
+  .service('scrollService', [function() {
+
+    this.scrollToIndex =
+
+      function(index) {
+        var height = document.body.clientHeight;
+        var thumb = document.getElementById('anchor' + index);
+        var trail = document.getElementById('phototrail');
+        var filler = document.getElementById('scrollbar-filler');
+
+        if (!thumb) return;
+
+        // Bugfix. See below.
+        filler.style.display = "initial";
+        trail.style.overflowY = "hidden";
+
+        if (thumb.offsetTop - height + thumb.clientHeight > trail.scrollTop) {
+          trail.scrollTop = (thumb.offsetTop - height + thumb.clientHeight);
+        } else
+          if (thumb.offsetTop < trail.scrollTop) {
+            trail.scrollTop = thumb.offsetTop - 20;
+          }
+
+        /*
+          It's a bugfix. Without it, if there's been a click on the list,
+          chrome makes a small scrolling adjustment after the above,
+          giving it a horrible double-scroll-jerk.
+          This was the only way I could find to fix it.
+          The filler means we can still have a scrollbar, it hides the
+          fact the scrollbar temporarily dissapears during this process.
+        */
+        setTimeout(function() {
+          trail.style.overflowY = "scroll";
+          filler.style.display = "none";
+        }, 1);
+
+      }
+  }])
